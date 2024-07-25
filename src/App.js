6bd3df08715c66f17c26a8384bdfde6e67a7ab6a
arifuleee <ariful.eee@gmail.com>
2024-07-26 04:48:42 +0600

    import components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Chatbot from './components/Chatbot'; // Add this import
+import Navbar from './components/Navbar';
+import AddPatientForm from './pages/AddPatientForm'; // Add this import
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Signup from './pages/Signup';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/chatbot" element={<Chatbot />} /> {/* Add this route */}
+          <Route path="/add-patient" element={<AddPatientForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
